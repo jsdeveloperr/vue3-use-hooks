2635acaf73d258ea4e53767e09ecc674cfdddc0f
Abdulnasır Olcan <abdulnasirolcan@Abdulnasr-Olcan-MacBook-Pro.local>
2022-07-16 23:05:50 +0300

    🚀 jest config js removed

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  transform: {
-    "^.+\\.js$": "babel-jest",
-    "^.+\\.vue$": "vue-jest",
-  },
-  moduleFileExtensions: ["vue", "js", "ts"],
-};
