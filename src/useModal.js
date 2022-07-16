dba3c5c2620bc99968400b1874d2d9ab98c79f4d
Abdulnasır Olcan <abdulnasirolcan@Abdulnasr-Olcan-MacBook-Pro.local>
2022-07-17 00:21:14 +0300

    🚀 useLocalStorage hooks and test added

--- src/useModal.js
+++ src/useModal.js
@@ -1,4 +1,9 @@
 import { useState } from './useState';
+/**
+ * This function is used in reactive modal.
+ * @param {initial} - The initial argument is the state used during the initial render.
+ * @returns - visible, setVisible, current, openModal, closeModal
+ */
 const useModal = (initItem = {}, initVisible = false) => {
     const [visible, setVisible] = useState(initVisible);
     const [current, setCurrent] = useState(initItem);
