f7f49039255b959bda1fb04c91ca25ba86a1ab7d
Abdulnasır Olcan <abdulnasirolcan@Abdulnasr-Olcan-MacBook-Pro.local>
2022-07-16 22:43:57 +0300

    🚀 example router added path

--- example/vue3/src/router/index.ts
+++ example/vue3/src/router/index.ts
@@ -1,29 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-const useModalComponent = () => import('../components/useModal.vue')
+const useModalComponent = () => import('../components/useModal.vue');
 const useStateComponent = () => import('../components/useState.vue');
 const useStringCaseComponent = () => import('../components/useStringCase.vue');
 
 const routes = [
   {
-      path: '/use-state-hook',
-      name: 'useState',
-      component: useStateComponent,
+    path: '/',
+    name: 'useStringCase',
+    component: useStringCaseComponent
   },
-    {
-        path: '/use-modal-hook',
-        name: 'useModal',
-        component: useModalComponent,
-    },
-    {
-      path: '/use-string-case-hook',
-      name: 'useStringCase',
-      component: useStringCaseComponent,
+  {
+    path: '/use-state-hook',
+    name: 'useState',
+    component: useStateComponent
   },
-]
+  {
+    path: '/use-modal-hook',
+    name: 'useModal',
+    component: useModalComponent
+  },
+  {
+    path: '/use-string-case-hook',
+    name: 'useStringCase',
+    component: useStringCaseComponent
+  }
+];
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
-})
+  history: createWebHistory(),
+  routes
+});
 
-export default router
+export default router;
